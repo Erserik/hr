d67434c9b842244a533283ed8d01d8f9f8aecfcc
Erserik <erserikdaryn82@gmail.com>
2025-04-14 22:42:38 +0500

    add admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Company, Vacancy, Response
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company_name', 'full_name', 'phone_number', 'industry', 'created_at')
+    search_fields = ('company_name', 'full_name', 'phone_number', 'industry')
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'job_title', 'specialization', 'city', 'hiring_plan', 'salary_min', 'salary_max')
+    search_fields = ('job_title', 'specialization', 'city')
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'position', 'experience')
+    search_fields = ('full_name', 'position', 'experience')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserRegistration, UserResume
 
-# Register your models here.
+@admin.register(UserRegistration)
+class UserRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'phone_number', 'created_at')
+    search_fields = ('full_name', 'phone_number')
+
+@admin.register(UserResume)
+class UserResumeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'full_name',
+        'position',
+        'specialization',
+        'work_format',
+        'expected_salary',
+        'experience'
+    )
+    search_fields = ('full_name', 'position', 'specialization', 'experience')
